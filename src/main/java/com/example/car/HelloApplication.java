6b70fece5330cb32b92d86a91707e9d2605aa03d
karim <karimtaha883@gmail.com>
2023-01-23 23:31:40 +0200

    final

--- src/main/java/com/example/car/HelloApplication.java
+++ src/main/java/com/example/car/HelloApplication.java
@@ -34,27 +34,27 @@ public class HelloApplication extends Application implements Initializable {
         final AudioClip hornClip = new AudioClip(this.getClass().getResource("horn.mp3").toExternalForm());
         final AudioClip crashClip = new AudioClip(this.getClass().getResource("crash.mp3").toExternalForm());
 
-        Rectangle rectangle1 = new Rectangle(0,0,40,40);
-        rectangle1.setArcHeight(10);
-        rectangle1.setArcWidth(10);
-        rectangle1.setFill(Color.RED);
-        Rectangle rectangle2 = new Rectangle(0,0,40,40);
-        rectangle2.setArcHeight(10);
-        rectangle2.setArcWidth(10);
-        rectangle2.setFill(Color.RED);
+        // Rectangle rectangle1 = new Rectangle(0,0,40,40);
+        // rectangle1.setArcHeight(10);
+        // rectangle1.setArcWidth(10);
+        // rectangle1.setFill(Color.RED);
+        // Rectangle rectangle2 = new Rectangle(0,0,40,40);
+        // rectangle2.setArcHeight(10);
+        // rectangle2.setArcWidth(10);
+        // rectangle2.setFill(Color.RED);
 
-        ImageView image1 = new ImageView(new Image(getClass().getResourceAsStream("car2.png")));
-        image1.setFitHeight(70);
-        image1.setFitWidth(70);
+        ImageView image1 = new ImageView(new Image(getClass().getResourceAsStream("car.png")));
+        image1.setFitHeight(60);
+        image1.setFitWidth(60);
         image1.setCursor(Cursor.HAND);
 
-        ImageView image2 = new ImageView(new Image(getClass().getResourceAsStream("car.png")));
+        ImageView image2 = new ImageView(new Image(getClass().getResourceAsStream("car1.png")));
         image2.setFitHeight(60);
         image2.setFitWidth(60);
         image2.setCursor(Cursor.HAND);
 
         PathElement[] path = {
-                new MoveTo(200, 50),
+                new MoveTo(100, 50),
                 new HLineTo(200),
                 new LineTo(300, 200),
                 new HLineTo(400),
@@ -80,10 +80,8 @@ public class HelloApplication extends Application implements Initializable {
         PathTransition pathTransition1 = new PathTransition();
         pathTransition1.setDuration(Duration.millis(9000));
         pathTransition1.setPath(road);
-        pathTransition1.setNode(rectangle1);
-        pathTransition1.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT); // keep the car
-                                                                                              // perpendicular to its
-                                                                                              // path
+        pathTransition1.setNode(image1);
+        pathTransition1.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT); // keep the car perpendicular to its path
         pathTransition1.setInterpolator(Interpolator.LINEAR); // linear interpolator - smooth animation
         pathTransition1.setCycleCount(Timeline.INDEFINITE); // Let the animation run forever
         pathTransition1.setAutoReverse(false); // Reverse direction on alternating cycles
@@ -93,10 +91,8 @@ public class HelloApplication extends Application implements Initializable {
         PathTransition pathTransition2 = new PathTransition();
         pathTransition2.setDuration(Duration.millis(9000));
         pathTransition2.setPath(road);
-        pathTransition2.setNode(rectangle2);
-        pathTransition2.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT); // keep the car
-                                                                                              // perpendicular to its
-                                                                                              // path
+        pathTransition2.setNode(image2);
+        pathTransition2.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT); // keep the car perpendicular to its path
         pathTransition2.setInterpolator(Interpolator.LINEAR); // linear interpolator - smooth animation
         pathTransition2.setCycleCount(Timeline.INDEFINITE); // Let the animation run forever
         pathTransition2.setAutoReverse(false); // Reverse direction on alternating cycles
@@ -121,19 +117,10 @@ public class HelloApplication extends Application implements Initializable {
             }
         });
 
-        // if (image1.getX() == image2.getX() || image1.getX() == image2.getX() - 30
-        //         || image1.getX() == image2.getX() + 30) {
-        //     System.out.println("inter");
-        // }
-
-        // if (image1.intersects(image2.getBoundsInParent())) {
-        //     System.out.println("inter");
-        // }
-
         AnimationTimer animationTimer = new AnimationTimer() {
             @Override
             public void handle(long l) {
-                if (rectangle1.getBoundsInParent().intersects(rectangle2.getBoundsInParent())) {       
+                if (image1.getBoundsInParent().intersects(image2.getBoundsInParent())) {
                     pathTransition1.setRate(pathTransition1.getRate() * -1);
                     pathTransition2.setRate(pathTransition2.getRate() * -1);
                     crashClip.play();
@@ -143,7 +130,7 @@ public class HelloApplication extends Application implements Initializable {
         animationTimer.start();
 
         Group root = new Group();
-        root.getChildren().addAll(road, divider, rectangle1, rectangle2);
+        root.getChildren().addAll(road, divider, image1, image2);
         root.setLayoutX(100);
         root.setLayoutY(50);
 
